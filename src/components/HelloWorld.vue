e9eff84dd04a96f68b8d0346f99f0145e10e335c
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-20 18:02:45 -0600

    Subiendo Carpeta

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup>
+// metodos
+</script>
+
+<template>
+  <!-- estructura html -->
+
+   <h2>Redes de computadoras</h2>
+   <img src="https://humanidades.com/wp-content/uploads/2018/10/redes-de-computadora-1-e1582281508636.jpg" alt="Descripción" width="300">
+   <p>Las redes de computadoras conectan dispositivos para compartir datos y recursos, mejorando la comunicación y eficiencia.</p>
+
+    <button>RED </button>
+    <button class="boton2">Inicio </button>
+
+   
+   
+</template>
+
+<style scoped>
+/* estilos css */
+.boton{
+
+border-radius: 15px;
+background-color:rgb(125, 19, 146);
+color: blueviolet;
+font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+h2{
+font-family: 'Times New Roman', Times, serif;
+font-size: 45px;
+color: rgb(83, 170, 32);
+}
+ h1{  
+  font-family:  Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 45px;
+  color: red;
+
+ }
+.btn{ 
+  border-radius: 15px;
+  background-color: #ffff;
+  color: black;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
