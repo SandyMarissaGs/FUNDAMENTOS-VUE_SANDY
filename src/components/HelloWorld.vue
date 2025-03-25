e44075bdb0f7765b8b01335b9f0017f5e440e770
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-24 18:33:34 -0600

    Actualizacion de cambios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,21 @@
 <script setup>
-// metodos
+// Metodos
 </script>
 
 <template>
-  <!-- estructura html -->
+  <!-- Estructura html -->
   <div class="container">
     <h1 class="text-center">La Biodiversidad</h1>
 
-
-    <div class="row justify-content-center">
-        <div class="col-md-14">
-            <div class="card">
-              <div class="card-header" style="text-align: center;">¿Qué es la biodiversidad?</div>
-                <div class="card-body">
-                    <p class="card-text">
-                        La biodiversidad es la variedad de vida en la Tierra, incluyendo la diversidad de especies, genes y ecosistemas. Es esencial para el equilibrio ecológico y el bienestar humano.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    
     <!-- INICIO DEL PRIMER ROW -->
     <div class="row mt-4">
-      <div class="col-md-4 mb-2">
+      <div class="col-md-6 mb-4">
         <div class="card h-100">
           <div class="card-header" style="text-align: center;">Diversidad de Especies</div>
           <div class="card-body">
             <p class="card-text">
-              <img src="https://www.imer.mx/ciudadana/wp-content/uploads/sites/16/BIODIVERSIDAD-1.jpg" class="card-img-top" alt="...">
+              <img src="https://www.imer.mx/ciudadana/wp-content/uploads/sites/16/BIODIVERSIDAD-1.jpg"
+              class="card-img-top" alt="340px" style="height: 340px;">
               La biodiversidad de especies se refiere a la variedad de organismos que coexisten en un ecosistema,
               compartiendo características y adaptaciones. Su clasificación permite estudiar sus relaciones biológicas y
               su impacto en el equilibrio ecológico.
@@ -38,27 +24,17 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4 mb-2">
-        <div class="card h-100">
-          <div class="card-header" style="text-align: center;">Diversidad Genetica</div>
-          <div class="card-body">
-            <p class="card-text">
-              <img src="https://aprendeencasa.sep.gob.mx/multimedia/B64images/202206/B64-IMG-0qly4Oarf7-RQ4nzpPxoE.png" class="card-img-top" alt="340px" style="height: 340px;">
-              Hace referencia a la variedad de genes que hay dentro de una misma especie. Cada una dispone de una cierta cantidad, la cual contribuye a garantizar la 
-              adaptación al entorno, es decir, tiene un impacto directo en la capacidad de supervivencia de los seres vivos. Cuanto mayor sea, menor es el riesgo de 
-              extinción. 
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-2">
+
+      <div class="col-md-6 mb-4">
         <div class="card h-100">
           <div class="card-header" style="text-align: center;">Diversidad de Ecosistemas</div>
           <div class="card-body">
             <p class="card-text">
-              <img src="https://i.pinimg.com/736x/b5/d7/d9/b5d7d9ba1c952374e563a233532e5ad7.jpg" class="card-img-top" alt="340px" style="height: 340px;">
-              Este nivel se refiere a las áreas geográficas donde se desarrollan los biomas, cada una con características que facilitan la supervivencia
-              de las especies. La diversidad de estos ecosistemas es crucial para mantener el equilibrio biológico, ya que todas las especies dependen de su entorno.  
+              <img src="https://i.pinimg.com/736x/b5/d7/d9/b5d7d9ba1c952374e563a233532e5ad7.jpg" class="card-img-top"
+                alt="340px" style="height: 340px;">
+                Este nivel abarca las áreas geográficas donde se desarrollan los biomas, cada una con condiciones que favorecen la supervivencia 
+                de las especies. La diversidad de ecosistemas es clave para el equilibrio biológico, ya que cada uno alberga diferentes formas de
+                 vida y sus interacciones.
             </p>
           </div>
         </div>
@@ -66,48 +42,78 @@
     </div>
     <!-- CIERRE DEL PRIMER ROW -->
 
-   <!-- INICIO DEL SEGUNDO ROW -->
-<div class="row mt-4">
-  <div class="col-md-4">
-    <div class="card" style="width: 18rem;">
-      <img src="https://thefoodtech.com/wp-content/uploads/2023/05/biosdiversidad-828x548.jpg" class="card-img-top" alt="...">
-      <div class="card-body text-center">
-        <p class="card-text">La diversidad alfa se refiere a la biodiversidad dentro de una zona específica, se define por la misma comunidad o ecosistema y el número de 
-          especies o grupos de especies, según la familia.</p>
-        <button class="btn btn-warning">Mas Información</button>
+    <!-- INICIO DEL SEGUNDO ROW -->
+    <div class="row mt-4">
+      <div class="col-md-3">
+        <div class="card h-100">
+          <img src="https://thefoodtech.com/wp-content/uploads/2023/05/biosdiversidad-828x548.jpg" 
+          class="card-img-top" alt="200px" style="height: 200px;">
+          <div class="card-body d-flex flex-column text-center">
+            <p class="card-text flex-grow-1">
+              La diversidad Alfa se refiere a la biodiversidad dentro de una zona específica, se define por la misma
+              comunidad o ecosistema y el número de
+              especies o grupos de especies, según la familia.
+            </p>
+            <button class="btn btn-warning mt-auto">Más Información</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100">
+          <img src="https://www.ambientum.com/wp-content/uploads/2018/09/ecosistema-animales-696x486.jpg"
+          class="card-img-top" alt="200px" style="height: 200px;">
+          <div class="card-body d-flex flex-column text-center">
+            <p class="card-text flex-grow-1">
+              La diversidad Beta se refiere a las interacciones ecológicas entre las especies de diferentes ecosistemas,
+              así como entre las especies dentro
+              de un mismo ecosistema a distintos niveles ambientales.
+            </p>
+            <button class="btn btn-warning mt-auto">Más Información</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100">
+          <img
+            src="https://www.lineaverdeguadalajara.com/lv/consejos-ambientales/biodiversidad/img/diferentes-ecosistemas.jpg" 
+            class="card-img-top" alt="200px" style="height: 200px;">
+          <div class="card-body d-flex flex-column text-center">
+            <p class="card-text flex-grow-1">
+              La diversidad Gamma se centra en la riqueza de los ecosistemas, describiendo las especies, grupos de
+              especies y recursos dentro de un
+              conjunto de ecosistemas de un mismo bioma. Los ecosistemas ofrecen beneficios de disfrutar de sus recursos
+              naturales y paisajes.
+            </p>
+            <button class="btn btn-warning mt-auto">Más Información</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card h-100">
+          <img src="https://aprendeencasa.sep.gob.mx/multimedia/B64images/202206/B64-IMG-0qly4Oarf7-RQ4nzpPxoE.png"
+          class="card-img-top" alt="200px" style="height: 200px;">
+          <div class="card-body d-flex flex-column text-center">
+            <p class="card-text flex-grow-1">
+              La diversidad Genética es la variabilidad de genes dentro de una especie, crucial para su adaptación y
+              supervivencia.
+              A mayor diversidad, menor riesgo de extinción, siendo el nivel más básico de la biodiversidad.
+            </p>
+            <button class="btn btn-warning mt-auto">Más Información</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="col-md-4">
-    <div class="card" style="width: 18rem;">
-      <img src="https://www.ambientum.com/wp-content/uploads/2018/09/ecosistema-animales-696x486.jpg" class="card-img-top" alt="...">
-      <div class="card-body text-center">
-        <p class="card-text">La diversidad Beta se refiere a las interacciones ecológicas entre las especies de diferentes ecosistemas, así como entre las especies dentro
-           de un mismo ecosistema a distintos niveles ambientales.</p>
-        <button class="btn btn-warning">Mas Información</button>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-md-4">
-    <div class="card" style="width: 18rem;">
-      <img src="https://www.lineaverdeguadalajara.com/lv/consejos-ambientales/biodiversidad/img/diferentes-ecosistemas.jpg" class="card-img-top" alt="...">
-      <div class="card-body text-center">
-        <p class="card-text">La diversidad Gamma se centra en la riqueza de los ecosistemas, describiendo las especies, grupos de especies y recursos dentro de un 
-          conjunto de ecosistemas de un mismo bioma. Los ecosistemas ofrecen beneficios de disfrutar de sus recursos naturales y paisajes.</p>
-        <button class="btn btn-warning">Mas Información</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- CIERRE DEL SEGUNDO ROW -->
+    <!-- CIERRE DEL SEGUNDO ROW -->
 
 
+    <!-- Espacio adicional -->
   </div>
+  <div class="mt-5"></div>
 </template>
 
 <style scoped>
 /* estilos css */
-
 </style>
