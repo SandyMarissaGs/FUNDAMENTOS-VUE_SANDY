509adabab42cda2f291ddf3d4933362bf9f0a958
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-24 19:21:44 -0600

    Agregue el Componente CardPrincipal y Importe a HelloWorld

---
+++ src/components/CardPrincipal.vue
@@ -0,0 +1,36 @@
+<template>
+        <!-- INICIO DEL PRIMER ROW -->
+        <div class="row mt-4">
+      <div class="col-md-6 mb-4">
+        <div class="card h-100">
+          <div class="card-header" style="text-align: center;">Diversidad de Especies</div>
+          <div class="card-body">
+            <p class="card-text">
+              <img src="https://www.imer.mx/ciudadana/wp-content/uploads/sites/16/BIODIVERSIDAD-1.jpg"
+              class="card-img-top" alt="340px" style="height: 340px;">
+              La biodiversidad de especies se refiere a la variedad de organismos que coexisten en un ecosistema,
+              compartiendo características y adaptaciones. Su clasificación permite estudiar sus relaciones biológicas y
+              su impacto en el equilibrio ecológico.
+              Resultado de procesos evolutivos, esta diversidad es esencial para la estabilidad ambiental.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 mb-4">
+        <div class="card h-100">
+          <div class="card-header" style="text-align: center;">Diversidad de Ecosistemas</div>
+          <div class="card-body">
+            <p class="card-text">
+              <img src="https://i.pinimg.com/736x/b5/d7/d9/b5d7d9ba1c952374e563a233532e5ad7.jpg" class="card-img-top"
+                alt="340px" style="height: 340px;">
+                Este nivel abarca las áreas geográficas donde se desarrollan los biomas, cada una con condiciones que favorecen la supervivencia 
+                de las especies. La diversidad de ecosistemas es clave para el equilibrio biológico, ya que cada uno alberga diferentes formas de
+                 vida y sus interacciones.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CIERRE DEL PRIMER ROW -->
+</template>
