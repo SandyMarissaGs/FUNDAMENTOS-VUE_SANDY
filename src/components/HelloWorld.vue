f33218328098014c28075598042c0110352b36c6
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-24 19:07:19 -0600

    Estructura del Segundo Row en el Componente Card.Vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 // Metodos
+import Card from './Card.vue';
 </script>
 
 <template>
@@ -42,76 +43,12 @@
     </div>
     <!-- CIERRE DEL PRIMER ROW -->
 
-    <!-- INICIO DEL SEGUNDO ROW -->
-    <div class="row mt-4">
-      <div class="col-md-3">
-        <div class="card h-100">
-          <img src="https://thefoodtech.com/wp-content/uploads/2023/05/biosdiversidad-828x548.jpg" 
-          class="card-img-top" alt="200px" style="height: 200px;">
-          <div class="card-body d-flex flex-column text-center">
-            <p class="card-text flex-grow-1">
-              La diversidad Alfa se refiere a la biodiversidad dentro de una zona específica, se define por la misma
-              comunidad o ecosistema y el número de
-              especies o grupos de especies, según la familia.
-            </p>
-            <button class="btn btn-warning mt-auto">Más Información</button>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <div class="card h-100">
-          <img src="https://www.ambientum.com/wp-content/uploads/2018/09/ecosistema-animales-696x486.jpg"
-          class="card-img-top" alt="200px" style="height: 200px;">
-          <div class="card-body d-flex flex-column text-center">
-            <p class="card-text flex-grow-1">
-              La diversidad Beta se refiere a las interacciones ecológicas entre las especies de diferentes ecosistemas,
-              así como entre las especies dentro
-              de un mismo ecosistema a distintos niveles ambientales.
-            </p>
-            <button class="btn btn-warning mt-auto">Más Información</button>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <div class="card h-100">
-          <img
-            src="https://www.lineaverdeguadalajara.com/lv/consejos-ambientales/biodiversidad/img/diferentes-ecosistemas.jpg" 
-            class="card-img-top" alt="200px" style="height: 200px;">
-          <div class="card-body d-flex flex-column text-center">
-            <p class="card-text flex-grow-1">
-              La diversidad Gamma se centra en la riqueza de los ecosistemas, describiendo las especies, grupos de
-              especies y recursos dentro de un
-              conjunto de ecosistemas de un mismo bioma. Los ecosistemas ofrecen beneficios de disfrutar de sus recursos
-              naturales y paisajes.
-            </p>
-            <button class="btn btn-warning mt-auto">Más Información</button>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <div class="card h-100">
-          <img src="https://aprendeencasa.sep.gob.mx/multimedia/B64images/202206/B64-IMG-0qly4Oarf7-RQ4nzpPxoE.png"
-          class="card-img-top" alt="200px" style="height: 200px;">
-          <div class="card-body d-flex flex-column text-center">
-            <p class="card-text flex-grow-1">
-              La diversidad Genética es la variabilidad de genes dentro de una especie, crucial para su adaptación y
-              supervivencia.
-              A mayor diversidad, menor riesgo de extinción, siendo el nivel más básico de la biodiversidad.
-            </p>
-            <button class="btn btn-warning mt-auto">Más Información</button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Card/>
     </div>
-    <!-- CIERRE DEL SEGUNDO ROW -->
 
-
-    <!-- Espacio adicional -->
   </div>
-  <div class="mt-5"></div>
+  
 </template>
 
 <style scoped>
